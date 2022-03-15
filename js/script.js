9a9074f24c1b357a11bd192c0111d7b9a5292afb
Marzio95 <marziodellarocca14@gmail.com>
2022-03-15 17:36:14 +0100

    applico vue nell html con il v-for

--- js/script.js
+++ js/script.js
@@ -2,5 +2,52 @@ const app = new Vue({
     el: '#root',
     data: {
 
+        arrayUtentiChat: [
+            {
+                nome: 'Michele',
+                imgUtente: 'avatar_1.jpg',
+                lastMessage: 'Ultimo messaggio inviato',
+            },
+            {
+                nome: 'Fabio',
+                imgUtente: 'avatar_2.jpg',
+                lastMessage: 'Ultimo messaggio inviato',
+            },
+            {
+                nome: 'Samuele',
+                imgUtente: 'avatar_3.jpg',
+                lastMessage: 'Ultimo messaggio inviato',
+            },
+            {
+                nome: 'Alessandro B.',
+                imgUtente: 'avatar_4.jpg',
+                lastMessage: 'Ultimo messaggio inviato',
+            },
+            {
+                nome: 'Alessandro L.',
+                imgUtente: 'avatar_5.jpg',
+                lastMessage: 'Ultimo messaggio inviato',
+            },
+            {
+                nome: 'Claudia',
+                imgUtente: 'avatar_6.jpg',
+                lastMessage: 'Ultimo messaggio inviato',
+            },
+            {
+                nome: 'Federico',
+                imgUtente: 'avatar_7.jpg',
+                lastMessage: 'Ultimo messaggio inviato',
+            },
+            {
+                nome: 'Davide',
+                imgUtente: 'avatar_8.jpg',
+                lastMessage: 'Ultimo messaggio inviato',
+            },
 
-    }})
+
+        ]
+
+
+    }
+}
+)
