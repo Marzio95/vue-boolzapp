ca44ae2744f1934edd35badde4fd91c64ebb5e39
Marzio95 <marziodellarocca14@gmail.com>
2022-03-15 18:32:14 +0100

    implemetno l inserimento di un messaggio

--- js/script.js
+++ js/script.js
@@ -2,11 +2,20 @@ const app = new Vue({
     el: '#root',
     data: {
 
+        newMessage: '',
         arrayUtentiChat: [
             {
                 nome: 'Michele',
                 imgUtente: 'avatar_1.jpg',
                 lastMessage: 'Ultimo messaggio inviato',
+                messaggiRicevuti: [
+                    'Hai portato a spasso il cane?',
+                    'Ricordati di i panni'  
+                ],
+                messaggiInviati: [
+                    'Tutto fatto!'
+                ]
+
             },
             {
                 nome: 'Fabio',
@@ -48,6 +57,21 @@ const app = new Vue({
         ]
 
 
+    }, methods: {
+        messaggioInviato() {
+            let  listaMessaggi = document.querySelector('.lista_messaggi')
+            let message = document.createElement('li');
+            message.classList.add('my_message');
+            message.innerHTML =   `<div> ${this.newMessage.trim()} </div> <span class="timing_message">12.00</span>`
+            if (this.newMessage.trim() == ''){
+                
+            } else {
+                listaMessaggi.append(message);
+            }
+            this.newMessage = '';
+            
+        }
     }
+
 }
 )
